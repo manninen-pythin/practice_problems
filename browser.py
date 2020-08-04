9e36964e8ff257fbbb2d7aa5971c8129a6e970cf
manninen-pythin <67717846+manninen-pythin@users.noreply.github.com>
2020-08-03 23:56:23 -0400

    Update browser.py

--- browser.py
+++ browser.py
@@ -1,41 +1,8 @@
 import sys
 import os
-
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.'''
-
-sites = {'bloomberg.com': bloomberg_com, 'nytimes.com': nytimes_com}
+import requests
+from bs4 import BeautifulSoup
+from colorama import init, Fore
 
 
 class TabOptions:
@@ -44,14 +11,13 @@ class TabOptions:
         self.pages = []
         self.page_index = -1
         self.dir = directory
+        self.sites = {}
 
     def operate(self):
-        while True:
+        while True:  # takes commands/ search terms and passes them to open page
             command = input()
-            print(self.pages)
-            print(self.page_index)
             if command == 'exit':
-                exit()
+                quit()
             if command == 'back':
                 if self.page_index <= 0:
                     self.refresh()
@@ -71,21 +37,23 @@ class TabOptions:
             self.open_page(command)
 
     def open_page(self, command):
-        if "." in command:
-            if command in sites:
-                print(sites[command])
-                page_name = ''.join(command.split(".")[:-1])
-                if page_name not in self.pages:
+        if "." in command:  # checks to see if program needs process a new search
+            site_info = FetchData(command).parse_text()  # passes command to fetch data
+            page_name = ''.join(command.rsplit(".", 1)[:-1])
+            if site_info != 'Error':
+                self.sites[page_name] = site_info  # if no error stores result in memory
+                print(self.sites[page_name])
+                if page_name not in self.pages:  # tab control
                     self.pages.append(page_name)
                     self.page_index += 1
                 with open(f"{self.dir}\\{page_name}", "w") as opened_tab:
-                    opened_tab.write(sites[command])
-                return
-        if command in self.pages:
+                    opened_tab.write(self.sites[page_name])
+            return
+        elif command in self.pages:
             with open(f"{self.dir}\\{command}", "r") as opened_tab:
                 for line in opened_tab.readlines():
                     print(line)
-            if self.pages[-1] != command:
+            if self.pages[-1] != command:  # tab control
                 self.pages.append(command)
                 self.page_index += 1
             return
@@ -98,8 +66,39 @@ class TabOptions:
             self.open_page(self.pages[self.page_index])
 
 
-dir_name = 'tb_tabs'
-if not os.path.exists(dir_name):
-    os.mkdir(dir_name)
-tab = TabOptions(dir_name)
-tab.operate()
+class FetchData:
+
+    def __init__(self, request):
+        self.request = request
+        self.url = ''
+        if not self.request.startswith('https://'):
+            self.url = self.url.join(('https://', self.request))
+
+    def parse_text(self):
+        r = requests.get(self.url)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, 'html.parser')
+            tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'ul', 'ol', 'li', 'a']
+            page_info = soup.find_all(tags)
+            parsed_text = ''
+            for p in page_info:
+                line = p.text
+                if p.name == 'a':
+                    line = Fore.BLUE + p.text
+                parsed_text += line + '\n'
+            return parsed_text
+        else:
+            return 'Error'
+
+
+def main():
+    init()
+    dir_name = sys.argv[1]
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    tab = TabOptions(dir_name)
+    tab.operate()
+
+
+if __name__ == '__main__':
+    main()
